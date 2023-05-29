2cd242338756dc82cbfbb5fe7e3cc5f34346cdab
avilbel <131410638+avilbel@users.noreply.github.com>
2023-05-29 18:16:07 -0300

    createdthe admin home

--- project/app/home/models.py
+++ project/app/home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class Tittles(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Writer(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
